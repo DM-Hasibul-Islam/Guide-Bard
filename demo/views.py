81c34556c8dcb4167fd6eea7400b62b21ac29e9e
D. M Hasibul Islam <dmhasibulislam233@gmail.com>
2022-10-09 23:03:43 +0600

    updated urls

---
+++ demo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def homePage(request):
+    return HttpResponse("<h1>This is HomePage.</h1>")
+
+
+def sign_up(request):
+    return HttpResponse("<h1>This is sign up Page.</h1>")
+
+
+def login(request):
+    return HttpResponse("<h1>This is login Page.</h1>")
+
+
+def tourist(request):
+    return HttpResponse("<h1>This is Tourist Page.</h1>")
+
+
+def tourist_guide(request):
+    return HttpResponse("<h1>This is Tourist Page for booking tour Guide.</h1>")
+
+
+def payment(request):
+    return HttpResponse("<h1>This is Tourist Page for payment.</h1>")
+
+
+def tour_guide(request):
+    return HttpResponse("<h1>This is Tour Guide's Page.</h1>")
+
+
+def emergency(request):
+    return HttpResponse("<h1>This is Emergency contacts page.</h1>")
