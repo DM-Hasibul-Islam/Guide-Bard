8ecb36d47e1bee5a1dfba0e8791dc7ed4386098d
DM-Hasibul-Islam <dmhasibulislam233@gmail.com>
2022-10-16 21:56:26 +0600

    update in models.py db.sqlite3 migration

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.contrib.auth import base_user
+
+
 # Create your models here.
+
+class login(models.Model):
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+
+class signup(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=30)
+    nationality = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    confirmPassword = models.CharField(max_length=30)
